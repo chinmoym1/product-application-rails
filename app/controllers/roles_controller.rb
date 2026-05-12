7b7b6f813e80229fc324caad0a9585e951fc7fa8
chinmoym1 <majeechinmoy786@gmail.com>
2026-05-12 16:09:45 +0530

    modified roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,18 +1,20 @@
 class RolesController < ApplicationController
   before_action :authenticate_user!
   
-  load_and_authorize_resource 
+  load_and_authorize_resource prepend: true
 
   def index
     @roles = current_user.company.roles
   end
 
   def new
-    @role = current_user.company.roles.build
+    @role.company = current_user.company
   end
 
   def create
-    @role = current_user.company.roles.build(role_params)
+    @role.company = current_user.company
+    
+    build_dynamic_permissions
 
     if @role.save
       redirect_to roles_path, notice: "Role was successfully created."
@@ -25,7 +27,11 @@ class RolesController < ApplicationController
   end
 
   def update
-    if @role.update(role_params)
+    @role.assign_attributes(role_params)
+    
+    build_dynamic_permissions
+
+    if @role.save
       redirect_to roles_path, notice: "Role was successfully updated."
     else
       render :edit, status: :unprocessable_entity
@@ -43,7 +49,21 @@ class RolesController < ApplicationController
 
   private
 
+  def build_dynamic_permissions
+    @role.permissions.destroy_all if @role.persisted?
+    @role.permissions.clear 
+
+    if params[:permissions].present?
+      params[:permissions].each do |subject_class, actions|
+        actions.each do |action|
+          next if action == "none"
+          @role.permissions.build(action: action, subject_class: subject_class)
+        end
+      end
+    end
+  end
+
   def role_params
-    params.require(:role).permit(:name, :description, permission_ids: [])
+    params.require(:role).permit(:name, :description)
   end
 end
