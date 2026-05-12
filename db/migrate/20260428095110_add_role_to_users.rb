6d9bff2a6989126920f76209aa9825d59f6adb3b
chinmoym1 <majeechinmoy786@gmail.com>
2026-05-12 12:43:51 +0530

    render error fixed

--- db/migrate/20260428095110_add_role_to_users.rb
+++ db/migrate/20260428095110_add_role_to_users.rb
@@ -1,9 +1,15 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.1]
+  class MigrationRole < ApplicationRecord
+    self.table_name = "roles"
+  end
+
   def up
     add_reference :users, :role, null: true, foreign_key: true
 
-    default_role = Role.find_or_create_by!(name: 'Cashier', description: 'For order management')
+    default_role = MigrationRole.find_or_create_by!(name: 'Cashier', description: 'For order management')
+    
     User.update_all(role_id: default_role.id)
+    
     change_column_null :users, :role_id, false
   end
 
