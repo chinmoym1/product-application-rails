ef52fc9c97343c6417b3d48b292de54186d07e8a
chinmoym1 <majeechinmoy786@gmail.com>
2026-05-12 16:50:18 +0530

    production port updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,12 +102,13 @@ Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
+      port:                 465,
       domain:               'gmail.com',
       user_name:            ENV['GMAIL_USERNAME'], 
       password:             ENV['GMAIL_PASSWORD'],      
       authentication:       'plain',
-      enable_starttls_auto: true
+      tls:                  true,
+      enable_starttls_auto: false
     }
-    
+
 end
